2bf90dd7fe58c30b7f5da4f7a16eb8c71f507958
Nick Prestel <nick.prestel@gmail.com>
2013-12-09 19:22:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cubert::Application.config.secret_key_base = 'c777843cf59f1861581941ae6b3000d5987910c55bb2d2f9d5997225d6d5229223747e32b5ba119d5003e0cb7db941ec04be5d939e4a39f9351ec718c4408409'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cubert::Application.config.session_store :cookie_store, key: '_cubert_session'
